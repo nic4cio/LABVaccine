5163e993c02c077c4e1bd51532d6dc6fb03611a3
Gabriel Nicácio <gabrielnn_1@live.com>
2024-02-09 10:09:56 -0300

    initial commit

---
+++ src/main/java/tech/devinhouse/labvaccine/LabVaccineApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labvaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabVaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabVaccineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/devinhouse/labvaccine/LabVaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labvaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabVaccineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
